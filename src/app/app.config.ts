a3234b44f15f1825f063da6cbc231d9d4b4f1220
dykhasall99-afk <dykhasall99@gmail.com>
2026-04-02 22:35:47 +0200

    Configuration HttpClient Angular et rootDir

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,9 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig} from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideHttpClient(),
   ]
 };
+ 
