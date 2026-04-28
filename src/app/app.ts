7db879115cbba5656397b2502b595def9b3fa00b
Syllayoussouf <youssousylla552@gmail.com>
2026-04-28 19:47:50 +0000

    ♻️  Ajout d’une horloge en temps réel avec fuseau horaire et validation de la recherche météo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Weather } from './services/weather';
 
@@ -10,9 +10,15 @@ import { Weather } from './services/weather';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App implements OnInit {
-  // Ville saisie par l’utilisateur (liée au champ input via ngModel)
+export class App implements OnInit, OnDestroy {
   city: string = '';
+  timezone: number | null = null;
+  intervalId: any;
+  time: string = '';
+  weatherData: any;
+  errorMessage: string = '';
+  emptyMessage: string = '';
+  isLoading: boolean = false;
 
   // Date du jour formatée en français (utilisée dans l’affichage)
   today = new Date().toLocaleDateString('fr-FR', {
@@ -22,42 +28,86 @@ export class App implements OnInit {
     day: 'numeric',
   });
 
-  weatherData: any;
-  errorMessage: string = '';
-  isLoading: boolean = false;
-
   constructor(
     private weatherService: Weather,
     private cdr: ChangeDetectorRef,
   ) {}
 
-  ngOnInit() {}
+  // Calcule et met à jour l'heure locale (ville ou appareil) puis force la détection des changements Angular
+  updateTime() {
+    const now = new Date();
 
-  closeError() {
-    this.errorMessage = '';
+    // Heure locale de l'utilisateur si champ vide
+    if (this.timezone === null) {
+      this.time = now.toLocaleTimeString('fr-FR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      });
+
+      // Heure de la ville saisie
+    } else {
+      const utc = now.getTime() + now.getTimezoneOffset() * 60000;
+      const cityTime = new Date(utc + this.timezone * 1000);
+      this.time = cityTime.toLocaleTimeString('fr-FR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      });
+    }
+
+    // Force la mise à jour de la vue (nécessaire car setInterval s'exécute hors du contexte Angular)
+    this.cdr.detectChanges();
   }
 
-  // Méthode appelée pour récupérer les données météo d’une ville
-  getWeather(city: string) {
-    this.isLoading = true;
+  /** Initialisation : affiche l'heure immédiatement, puis la rafraîchit chaque seconde via setInterval */
+  ngOnInit() {
+    this.updateTime();
+    this.intervalId = setInterval(() => {
+      this.updateTime();
+    }, 1000);
+  }
+
+  /** Destruction : stoppe l'intervalle pour éviter les fuites mémoire quand le composant est détruit */
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+
+  getWeather() {
+    const cityName = this.city.trim();
+
+    // Si champ vide -> affiche avertissement
+    if (!cityName) {
+      this.emptyMessage = "Veuillez entrez le nom d'une ville !";
+      // Efface le message après 3 secondes
+      setTimeout(() => {
+        this.emptyMessage = '';
+        this.cdr.detectChanges();
+      }, 3000);
+      return;
+    }
+
+    // Réinitialisation des états avant l'envoi de la requête
+    ((this.emptyMessage = ''), (this.isLoading = true));
     this.errorMessage = '';
     this.weatherData = null;
 
-    // Appel au service météo (Observable)
-    this.weatherService.getWeather(city).subscribe({
-      // Cas de succès : données reçues
-      next: (data) => {
+    // Appel au service météo
+    this.weatherService.getWeather(cityName).subscribe({
+      next: (data: any) => {
         this.weatherData = data;
+        this.timezone = data.timezone; // Récupère le décalage UTC de la ville pour recalculer l'heure locale
         this.isLoading = false;
-        // Forcer la mise à jour de la vue (utile si Angular ne détecte pas le changement)
-        this.cdr.detectChanges();
       },
-
-      // Cas d’erreur : problème API ou réseau
-      error: (err) => {
-        this.errorMessage = 'Erreur lors de la récupération des données';
+      error: () => {
+        this.emptyMessage;
+        this.errorMessage = 'Ville introuvable. Vérifiez le nom et réessayez.';
+        // Efface le message après 3 secondes
+        setTimeout(() => {
+          this.errorMessage = '';
+          this.cdr.detectChanges();
+        }, 3000);
         this.isLoading = false;
-        this.cdr.detectChanges();
       },
     });
   }
